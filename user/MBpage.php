5f6494290489b40b290216b6bfb257e1ca47576f
Lynn Nguyen <ngocngan.9a2.2015@gmail.com>
2022-07-25 17:13:21 -0700

    Converted from html to php files

--- user/MBpage.php
+++ user/MBpage.php
@@ -95,12 +95,4 @@
     </section>
     <!-- End Main Content Section -->
 
-    <!-- Footer Section -->
-    <section class="footer">
-        <div class="container text-center">
-            <p class="footer-text">2022 All rights reserved, PC Components Tracker. Designed by Group 1</p>
-        </div>
-    </section>
-    <!-- End Footer Section -->
-</body>
-</html>
+<?php include('partials-frontend/footer.php'); ?>
---
+++ user/partials-frontend/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <title>Home Page - PC Components Tracker</title>
+        <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="../css/util.css">
+    </head>
+
+    <body>
+        <!-- Header Section -->
+        <!-- Include Logo and Menu Option-->
+        <section class="header">
+            <div class="container">
+                <div class="logo">
+                    <a href="#" title="Logo">
+                        <img src="../images/header-logo.png" alt="Website Logo" class="img-contain">
+                    </a>
+                </div>
+
+                <div class="menu text-right">
+                    <ul>
+                        <li>
+                            <a href="index.html">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">Components</a>
+                        </li>
+                        <li>
+                            <a href="#">Log In</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+        </section>
+        <!-- End Header Section -->
