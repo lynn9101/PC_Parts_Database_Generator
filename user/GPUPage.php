30f44dc27328976e6342b3e907fdac47914d88b3
the-monitor-lizard <justinyan888@gmail.com>
2022-07-29 01:45:50 -0700

    add and remove files as necessary 2

---
+++ user/GPUPage.php
@@ -0,0 +1,74 @@
+<?php include('partials-frontend/header.php'); ?>
+
+    <!-- Banner Section -->
+    <div class="product-banner">
+        <div class="product-banner-text">
+            <h1>GPU</h1>
+        </div>
+    </div>
+    <!-- End Banner Section -->
+
+    <!-- Main Content Section -->
+    <section class="product-main-content">
+        <div class="filter-selection">
+            <div class="filter">
+                <h3>></h3>
+                <h3>Model</h3>
+            </div>
+            <div class="filter">
+                <h3>></h3>
+                <h3>Memory Size</h3>
+            </div>
+            <div class="filter">
+                <h3>></h3>
+                <h3>Memory Type</h3>
+            </div>
+            <div class="filter">
+                <h3>></h3>
+                <h3>Boost Clock</h3>
+            </div>
+            <div class="filter">
+                <h3>></h3>
+                <h3>Core Clock</h3>
+            </div>
+            <div class="filter">
+                <h3>></h3>
+                <h3>Integrated/Dedicated</h3>
+            </div>
+        </div>
+        
+        <div class="results-section">
+            <table>
+                <tr>
+                    <th>Part ID</th>
+                    <th>Model</th>
+                    <th>Memory Size</th>
+                    <th>Memory Type</th>
+                    <th>Boost Clock</th>
+                    <th>Core Clock</th>
+                    <th>Integrated/Dedicated</th>
+                </tr>
+                <tr>
+                    <td>712385479</td>
+                    <td>Black</td>
+                    <td>15Db</td>
+                    <td>Intel</td>
+                    <td>10</td>
+                    <td>10</td>
+                    <td>Dedicated</td>
+                </tr>
+                <tr>
+                    <td>8782348</td>
+                    <td>Black</td>
+                    <td>15Db</td>
+                    <td>Intel</td>
+                    <td>10</td>
+                    <td>10</td>
+                    <td>Dedicated</td>
+                </tr>
+            </table>
+        </div>
+    </section>
+    <!-- End Main Content Section -->
+
+<?php include('partials-frontend/footer.php'); ?>
