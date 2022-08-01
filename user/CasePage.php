3df229a67a2c731b7c9b8caf020e7c80e7a830ee
jca465 <jca465@sfu.ca>
2022-07-31 23:24:29 -0700

    FINALLY FINISHED FILTERS filter mechanics are confined to only 1 choice per attribute

--- user/CasePage.php
+++ user/CasePage.php
@@ -12,11 +12,13 @@
     <!-- Main Content Section -->
     <section class="product-main-content">
         <div class="filter-selection">
-        <form action="" method="POST">
+
+        <form action="http://localhost/pc_parts_database_generator/user/CasePage.php" method="POST">
             <div class="filter">
                 <h3>Colour</h3>
             </div>
-            <ul>
+            <select name="color" class="filter-dropdown">
+                <option value="0">All</option>
                 <?php
                     // Query to get all suppliers in the database
                     $sql = "SELECT DISTINCT colour 
@@ -29,16 +31,12 @@
                         $numRows = mysqli_num_rows($result);
 
                         if ($numRows > 0) {
-                            $count = 0;
+                            $count = 1;
                             while ($rows = mysqli_fetch_assoc($result)) {
                                 // Get data from each row
                                 $color = $rows['colour'];
-                                $inputID = "color$count";
                                 ?>
-                                <li class="filter-item">
-                                    <input type="checkbox" id=<?php echo $inputID?> name=<?php echo $inputID?> value=<?php echo $count?>>
-                                    <label for=<?php echo $inputID?>><?php echo $color?></label>
-                                </li>
+                                <option value=<?php echo $color?>><?php echo $color?></option>
 
                                 <?php
                                 $count++;
@@ -46,11 +44,12 @@
                         }
                     }
                     ?>
-            </ul>
+            </select>
             <div class="filter">
                 <h3>Form Factor</h3>
             </div>
-            <ul>
+            <select name="ff" class="filter-dropdown">
+                <option value="0">All</option>
                 <?php
                     // Query to get all suppliers in the database
                     $sql = "SELECT DISTINCT formfactor 
@@ -62,17 +61,13 @@
                         $numRows = mysqli_num_rows($result);
 
                         if ($numRows > 0) {
-                            $count = 0;
+                            $count = 1;
                             while ($rows = mysqli_fetch_assoc($result)) {
                                 // Get data from each row
                                 $ff = $rows['formfactor'];
-                                $inputID = "ff$count";
                                 ?>
 
-                                <li class="filter-item">
-                                    <input type="checkbox" id=<?php echo $inputID?> name=<?php echo $inputID?> value=<?php echo $count?>>
-                                    <label for=<?php echo $inputID?>><?php echo $ff?></label>
-                                </li>
+                                <option value="<?php echo $ff?>"><?php echo $ff?></option>
 
                                 <?php
                                 $count++;
@@ -80,7 +75,7 @@
                         }
                     }
                     ?>
-            </ul>
+            </select>
             <div class="confirm-section">
                 <input type="submit" name="confirm-filter" value="Confirm" class="confirm-btn">
             </div>
@@ -88,10 +83,71 @@
         </div>
 
         <div class="results-section">
-            <?php 
-                $query = "WHERE c2.colour='Silver'";
-                include('table-assets/caseTable.php');
-            ?>
+            <table>
+                <tr>
+                    <th>Model</th>
+                    <th>Colour</th>
+                    <th>Form Factor</th>
+                </tr>
+                <?php
+
+                    $cond = [];
+                    if(isset($_POST['color'])){
+                        $color = 'c2.colour = "' . $_POST['color'] . '"';
+                        if($_POST['color'] != 0){
+                            array_push($cond, $color);
+                        }
+                    }
+                    if(isset($_POST['ff'])){
+                        $ff = 'c1.formfactor = "' . $_POST['ff'] . '"';
+                        if($_POST['ff'] != 0){
+                            array_push($cond, $ff);
+                        }
+                    }
+
+                    $condStr = "";
+                    if(count($cond) > 0){
+                        $condStr = "WHERE ";
+                        for($x = 0; $x < count($cond); $x++){
+                            $condStr .= $cond[$x];
+                            if($x + 1 != count($cond)){
+                                $condStr .= " OR ";
+                            }
+                        }
+                    }
+
+                    // Query to get all suppliers in the database
+                    $sql = "SELECT c1.modelname model, c1.formfactor ff, c2.colour color 
+                    FROM case1 c1
+                    INNER JOIN case2 c2
+                    ON c1.modelname = c2.modelname $condStr";
+
+                    $result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+
+                    if ($result == TRUE) {
+                        // Count the number of rows needed in the table
+                        $numRows = mysqli_num_rows($result);
+
+                        if ($numRows > 0) {
+                            while ($rows = mysqli_fetch_assoc($result)) {
+                                // Get data from each row
+                                $model = $rows['model'];
+                                $color = $rows['color'];
+                                $ff = $rows['ff'];
+                                ?>
+
+                                <tr>
+                                    <td><?php echo $model; ?></td>
+                                    <td><?php echo $color; ?></td>
+                                    <td><?php echo $ff; ?></td>
+                                </tr>
+
+                                <?php
+                            }
+                        }
+                    }
+                ?>
+            </table>
         </div>
     </section>
     <!-- End Main Content Section -->
